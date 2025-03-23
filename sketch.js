4fb7c5eb7e33beb759abf34ed093c91b3622d86f
poor1314 <poor1314@gmail.com>
2025-03-22 22:30:41 -0500

    almost complete

--- sketch.js
+++ sketch.js
@@ -2,82 +2,75 @@
 
 //when people open the website, there should be a default grid 16 x 16
 const container = document.querySelector(".container");
-let pixel;
+const slider = document.querySelector("#myRange");
 
-for (let i=0; i < 16; i++){
-    pixel = document.createElement("div");
+for (let i = 0; i < 16; i++){
+    let pixel = document.createElement("div");
     pixel.className = "pixel";  
     container.append(pixel);
 }
 
-// some bug with mouse event
-// read more about mouse event
 container.addEventListener("mouseover", (e) =>{
-    console.log("mouseOver")
+    
+    // gets css property(opacity) of current target 
+    opacityValue = Number(window.getComputedStyle(e.target).getPropertyValue("opacity"))
+   
     if (e.target.className === "pixel"){
         const randomColor = getRandomColor()
         e.target.style.backgroundColor = randomColor;
-        
+    
+        // why did incremental only works when i put  opacityValue += 0.1 before  e.target.style.opacity =  opacityValue
+        opacityValue += 0.3;
+        e.target.style.opacity =  opacityValue;
     }
-
-    let getPixel = document.querySelector(".pixel")
-    getPixel.style.opacity = 1;
-
 });
 
-// container.addEventListener("mouseover", () =>{
-//         // gets the current opacity value of the pixel
 
-        
-//         let getPixel = document.querySelector(".pixel")
-//         // let pixelOpacity = window.getComputedStyle(getPixel).getPropertyValue('opacity');
-//         // pixelOpacity += 0.1;
-//         // getPixel.style.opacity = pixelOpacity
 
-//         // getPixel.style.opacity = '0.1';
-//         // opacity = parseFloat(getPixel.style.opacity);
-//         // getPixel.style.opacity = opacity + 0.1;
-//         getPixel.style.opacity = 1;
-// });
-
-document.querySelector(".button").addEventListener("click", () => {
-   
-    const input = document.querySelector(".input");
-    
-    // console.log("the input is:",input.value)
-    // console.log(typeof(Number(input.value)))
-
-    const isNumber = Number(input.value)
-    
-    if (Number.isInteger(isNumber) && isNumber >= 1 && isNumber <= 99){
-        console.log("If-statement has passed");
-
+function newGrid(e){
+    const isNumber = Number(e.target.value);
+    if (Number.isInteger(isNumber) && isNumber >= 1 && isNumber <= 100){
         // remove whole grid
         let parent = document.querySelector(".container");
         parent.replaceChildren(); 
-
+        
 
         const flexValue = 100/isNumber;
         // Math.pow(isNumber, 2)
         // if given input is 3, 
         // Math.pow(3, 2) = 9, correctly represent a 3 x 3 grid
         for (let i = 0 ; i < Math.pow(isNumber, 2); i++){
-           
+            
             let pixel = document.createElement("div");
             pixel.className = "pixel";  
             pixel.style.flex = `${flexValue}%`
             pixel.style.height = `${flexValue}%`
             container.append(pixel);
         }
+    }
+}
 
-        // prompt user to use an integer
-        }else console.log("try again!");
+// two events triggers the same 
+
+// slider.addEventListener("click",newGrid)
+// slider.addEventListener("mouseover",newGrid)
+["click", "mouseover"].forEach(e => {
+    slider.addEventListener(e, newGrid);
+})
 
 
-    input.value = "";
-    input.focus();
-    
-});
+
+function sliderNumber(e){
+    const getText = document.querySelector(".text")
+    getText.textContent = `${e.target.value} x ${e.target.value}`;
+}
+
+
+["pointermove", "click"].forEach(e => {
+    slider.addEventListener(e, sliderNumber);
+})
+
+
 
 function getRandomColor() {
     let r = Math.floor(Math.random() * 256); // Random between 0-255
